20175e0d1a4c2b8b645192ea37ea1df3c76cbac3
Jean Paul <jeanpaulwebb@gmail.com>
2017-11-20 17:56:21 -0200

    Heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
+/**
+ * Imports Angular
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 import {AppComponent} from './app.component';
 import {MaterializeModule} from "angular2-materialize";
 import {PerfilComponent} from './perfil/perfil.component';
 import {AppRoutingModule} from "./app-routing.module";
-import {FormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
-import { ListColaboradoresComponent } from './list-colaboradores/list-colaboradores.component';
+import {ListColaboradoresComponent} from './list-colaboradores/list-colaboradores.component';
 /**
  * Imports para: Google Maps (AGM)
  * AIzaSyDY2jvvjH4vZ-yDxnRJYyDzpWqa_0QtQXU
